80cea14b545e90cb2f562ef8ef4e3b63d9528b1b
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2019-04-28 21:22:49 +0900

    change start script names

--- command/start-apache-hadoop-pseudo-yarn.sh
+++ command/start-apache-hadoop-pseudo-yarn.sh
@@ -4,5 +4,3 @@
 su hadoop -c "${HADOOP_HOME}/sbin/start-dfs.sh"
 su hadoop -c "${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /user/hadoop"
 su hadoop -c "${HADOOP_HOME}/sbin/start-yarn.sh"
-
-tail -f /dev/null
--- command/start-apache-hadoop-pseudo.sh
+++ command/start-apache-hadoop-pseudo.sh
@@ -3,5 +3,3 @@
 /etc/init.d/ssh start
 su hadoop -c "${HADOOP_HOME}/sbin/start-dfs.sh"
 su hadoop -c "${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /user/hadoop"
-
-tail -f /dev/null
